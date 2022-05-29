8e5707550b6b6ac6e92ce948a68eabf9f28a0b2c
Art-Net <53600019+4rt-Net@users.noreply.github.com>
2022-05-29 19:41:13 +0200

    Added Help Menu & Small interface/command changes
    Added the help menu
    Quickfix help menu already working - others not yet implemented. 
    Fixed minor problems with entry boxes. 
    Added a target entry box to connect to a target

--- Art-Tool.py
+++ Art-Tool.py
@@ -49,7 +49,7 @@ def HelpMenuExit():
 def QuickfixCommand():
 	Handler = paramiko.SSHClient()
 	Handler.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	Handler.connect(IPEntryBox.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
+	Handler.connect(targetIPAddress.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
 	stdin, stdout, stderr = Handler.exec_command('ip service set ftp disabled=yes')
 	stdin, stdout, stderr = Handler.exec_command('ip service set api disabled=yes')
 	stdin, stdout, stderr = Handler.exec_command('ip service set api-ssl disabled=yes')
@@ -57,19 +57,20 @@ def QuickfixCommand():
 	stdin, stdout, stderr = Handler.exec_command('system ntp client set enabled=yes primary-ntp=196.4.160.4')
 	stdin, stdout, stderr = Handler.exec_command('ip socks set enabled=no')
 	stdin, stdout, stderr = Handler.exec_command('ip dns cache flush')
+	stdin, stdout, stderr = Handler.exec_command('ip proxy set enabled=no')
 	Handler.close()
 
 def SendScript():
 	Handler = paramiko.SSHClient()
 	Handler.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	Handler.connect(IPEntryBox.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
+	Handler.connect(targetIPAddress.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
 	stdin, stdout, stderr = Handler.exec_command(ScriptEntryBox.get())
 	Handler.close()
 
 def SecurityPortKnocker():
 	Handler = paramiko.SSHClient()
 	Handler.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	Handler.connect(IPEntryBox.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
+	Handler.connect(targetIPAddress.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
 	stdin, stdout, stderr = Handler.exec_command('ip firewall address-list add list=Temporary')
 	stdin, stdout, stderr = Handler.exec_command('ip firewall address-list add list=Secured')
 	stdin, stdout, stderr = Handler.exec_command('ip firewall filter add action=add-src-to-address-list address-list=Temporary address-list-timeout=25s chain=input comment="Temp Auth" disabled=yes dst-port=4444 protocol=tcp')
@@ -82,7 +83,7 @@ def SecurityPortKnocker():
 def SecserviceRestrict():
 	Handler = paramiko.SSHClient()
 	Handler.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	Handler.connect(IPEntryBox.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
+	Handler.connect(targetIPAddress.get(), username=UsernameEntryBox.get(), password=PasswordEntryBox.get())
 	stdin, stdout, stderr = Handler.exec_command('ip service set telnet address='+RestrictionIP)
 	stdin, stdout, stderr = Handler.exec_command('ip service set ftp address='+RestrictionIP)
 	stdin, stdout, stderr = Handler.exec_command('ip service set www address='+RestrictionIP)
@@ -109,6 +110,7 @@ def SecurityRestrictIPMenu():
 
 
 def SecurityMenu():
+	global SecurityMenuTopLevel
 	SecurityMenuTopLevel = tk.Toplevel(root)
 	SecurityMenuTopLevel.geometry("400x200")
 	SecurityMenuTopLevel.title("Security")
@@ -153,6 +155,36 @@ def ScriptingMenu():
 	ScriptExitbutton = tk.Button(ScriptingMenuToplevel, text='back', font='Arial',fg=fg1Red, command=ScriptingMenuExit)
 	ScriptExitbutton.place(x=200,y=200, width=180, height=30)
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+#Help menu command list and menu
+def HelpMenu():
+	global CHelpMenu
+	global Helpdrop
+	CHelpMenu = tk.Toplevel(root)
+	CHelpMenu.geometry("400x200")
+	CHelpMenu.title("Help Menu")
+	CHelpMenu.configure(bg=fg3Black)
+	HelpMLabel = Label(CHelpMenu, image = Backdrop)
+	HelpMLabel.place(x=0,y=0)
+	HelpMLabelOptions = tk.Label(CHelpMenu, text="Select an option to view information about its usage:", font=font1, fg=fg5White, bg=fg3Black)
+	HelpMLabelOptions.place(x=15,y=50)
+
+	#Combo box configuration (Drop down menu)
+	Helpdrop = ttk.Combobox(CHelpMenu, value=options2)
+	Helpdrop.current(0)
+	Helpdrop.bind("<<ComboboxSelected>>", HelpCommand)
+	Helpdrop.place(x=130,y=85,width=160,height=30)
+
+def QuickfixHelp():
+	QuickFixHelpMenu = tk.Toplevel(root)
+	QuickFixHelpMenu.geometry("520x275")
+	QuickFixHelpMenu.title("QuickFix Details")
+	QuickFixHelpMenu.configure(bg=fg3Black)
+	QuickFixBackground = Label(QuickFixHelpMenu, image = Backdrop)
+	QuickFixBackground.place(x=0,y=0)
+	QuickFixHelpLabel = tk.Label(QuickFixHelpMenu, fg=fg5White, bg=fg3Black,text="The Quickfix command can be used to fix common problems found while troubleshooting.\n\nCurrently the command does the following:\n\n- Disable FTP service\n- Disable API service\n- Disable API-SSL service\n- Enables DNS Allow Remote Requests\n- Flushes DNS Cache\n- Fixes SNTP IP address\n- Disables Socks if enabled\n- Disables Web Proxy\n\n\nNote:\nQuickfix can sometimes fail to execute fully\nThis happens because it pushes a large amount of commands to the routerboard simultaneously.\nIf the command throws an exception, just execute it again and it should execute properly.")
+	QuickFixHelpLabel.place(x=1,y=1)
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 #Main menu linked commands
 def MainCommand(event):
@@ -164,14 +196,28 @@ def MainCommand(event):
 		SecurityMenu()
 	elif drop.get() == "Scripting":
 		ScriptingMenu()
+	elif drop.get() == "Help":
+		HelpMenu()
+
+def HelpCommand(event):
+	if Helpdrop.get() == "Quickfix Details":
+		QuickfixHelp()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 #Options for the tool (Command set)
+options2 = [
+"Quickfix Details",
+"Masquerade Usage",
+"Security Usage",
+"Scripting Usage",
+]
+
 options = [
 "Quickfix",
 "Masquerade",
 "Security",
 "Scripting",
+"Help"
 ]
 
 #Clicked variable for the dropdown box
@@ -190,6 +236,12 @@ PasswordLabel.place(x=5,y=70,width=150,height=30)
 PasswordEntryBox = tk.Entry(root, textvariable="", font=font1, fg=fg3Black, bg=fg6Grey)
 PasswordEntryBox.place(x=5,y=100,width=150,height=30)
 
+#IP address for target entry field and label
+TargetIPLabel = tk.Button(root, text="Target IP:", font=font1, fg=fg5White, bg=fg3Black)
+TargetIPLabel.place(x=5,y=150,width=150,height=30)
+targetIPAddress = tk.Entry(root, textvariable="", font=font1, fg=fg3Black, bg=fg6Grey)
+targetIPAddress.place(x=155,y=150,width=200,height=30)
+
 #Combo box configuration (Drop down menu)
 drop = ttk.Combobox(root, value=options)
 drop.current(0)
@@ -198,12 +250,11 @@ drop.place(x=250,y=10,width=150,height=30)
 
 
 class TheBox:
-	def __init__(self, IPEntryBox, ScriptEntryBox, UsernameEntryBox, PasswordEntryBox, RestrictionIP):
-		self.IPEntryBox = IPEntryBox
+	def __init__(self,targetIPAddress ,ScriptEntryBox, UsernameEntryBox, PasswordEntryBox, RestrictionIP):
 		self.ScriptEntryBox = ScriptEntryBox
 		self.UsernameEntryBox = UsernameEntryBox
 		self.PasswordEntryBox = PasswordEntryBox
-#		self.SNTPIP = SNTPIP
+		self.targetIPAddress = targetIPAddress
 #		self.MasqueradeIP = MasqueradeIP
 		self.RestrictionIP = RestrictionIP
 
